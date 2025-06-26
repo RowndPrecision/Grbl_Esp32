f4c989c3ed85ea5280c37d2000d521d5ec7834d2
OzanTRownd <ozan.tayar@rowndprecision.com>
2025-06-26 12:28:58 +0300

    v3.8.0 - servo version

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -21,8 +21,8 @@
 */
 
 // Grbl versioning system
-const char* const CNC_VERSION        = "3.7.1";
-const char* const CNC_VERSION_BUILD  = "20250625";
+const char* const CNC_VERSION        = "3.8.0s";
+const char* const CNC_VERSION_BUILD  = "20250626";
 const char* const GRBL_VERSION       = "1.3a";
 const char* const GRBL_VERSION_BUILD = "20211103";
 
