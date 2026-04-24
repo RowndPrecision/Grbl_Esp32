276176fffcdbb8d1e694925377413a425535fab6
OzanTRownd <ozan.tayar@rowndprecision.com>
2026-04-24 15:27:17 +0300

    v4.2.0 - Main version -refactor limit reporting functionality

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -21,7 +21,7 @@
 */
 
 // Grbl versioning system
-const char* const CNC_VERSION        = "4.2.0s";
+const char* const CNC_VERSION        = "4.2.0";
 const char* const CNC_VERSION_BUILD  = "20260424";
 const char* const GRBL_VERSION       = "1.3a";
 const char* const GRBL_VERSION_BUILD = "20211103";
