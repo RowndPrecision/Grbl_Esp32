bd49960f691c4a7a2a2393b7436ff66c224820cc
OzanTRownd <ozan.tayar@rowndprecision.com>
2026-07-20 13:05:47 +0300

    v4.3.0 - Main version

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -21,7 +21,7 @@
 */
 
 // Grbl versioning system
-const char* const CNC_VERSION        = "4.3.0s";
+const char* const CNC_VERSION        = "4.3.0";
 const char* const CNC_VERSION_BUILD  = "20260720";
 const char* const GRBL_VERSION       = "1.3a";
 const char* const GRBL_VERSION_BUILD = "20211103";
